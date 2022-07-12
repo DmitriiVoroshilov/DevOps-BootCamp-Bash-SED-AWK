b38d9a759042345f6ce41900b65b253e7d99a6f8
Dmitrii Voroshilov <Dmitrii_Voroshilov@epam.com>
2022-07-12 14:34:42 +0300

    rev 0.3

--- sedawk.sh
+++ sedawk.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # Create copy of passwd file to passwd_new.
-# cp passwd passwd_new
-cat passwd > passwd_new
+cp passwd passwd_new
 
-# Change shell for user saned
-awk -i inplace '/saned/ {gsub("/usr/sbin/nologin", "/bin/bash")}1' passwd_new
+# Change shell for user saned and send to pipe
+awk 'BEGIN {FS = OFS = ":"} {if ($1 == "saned") $7 = "/bin/bash"} {print $0}' passwd |
 
-# Change shell for user avahi
-sed -i '/avahi/s/\/usr\/sbin\/nologin/\/bin\/bash/' passwd_new
+# Change shell for user avahi and send to pipe
+sed '/avahi/s/\/usr\/sbin\/nologin/\/bin\/bash/' |
 
-# Save only 1-st 3-th 5-th 7-th columns of each string
-awk -i inplace -F ":" '{print $1 ":" $3 ":" $5 ":" $7}' passwd_new
+# Save only 1-st 3-th 5-th 7-th columns of each string and send to pipe
+awk 'BEGIN {FS = OFS = ":"} {print $1,$3,$5,$7}' |
 
-# Remove all lines containing word "daemon"
-sed -i '/daemon/d' passwd_new
+# Remove all lines containing word "daemon" and send to pipe
+sed '/daemon/d' |
 
-# Change shell for all users with even UID to /bin/zsh
-awk -i inplace 'BEGIN {FS = OFS = ":"} {if ($2 % 2 == 0) $4 = "/bin/zsh"} {print $0}' passwd_new
+# Change shell for all users with even UID to /bin/zsh and write output to file
+awk 'BEGIN {FS = OFS = ":"} {if ($2 % 2 == 0) $4 = "/bin/zsh"} {print $0}' > passwd_new
+
+# end of torment. god damn!
